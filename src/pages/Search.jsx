a2817449351992cf620ecd5e4a69ec91817304a5
Xue Fei <xue.f1@northeastern.edu>
2025-09-09 22:14:48 -0400

    Modifying english

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -146,7 +146,7 @@ const Search = ({ hideTitle }) => {
     if (!isNaN(pageNumber) && pageNumber >= 1 && pageNumber <= totalPages) {
       setPage(pageNumber);
     } else {
-      alert(`请输入 1 到 ${totalPages} 之间的页码！`);
+      alert(`Please enter a page number between 1 and ${totalPages}!`);
     }
     setInputPage("");
   };
